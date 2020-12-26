f07ab1c18305cda18f17c3c6f2289299310b1d10
ShibashisBasak <ShibashisBasak@DESKTOP-73NPAR6>
2020-12-26 21:24:44 +0530

    Selenium Projects

--- Activity1/src/projectSelenium/Activity9.java
+++ Activity1/src/projectSelenium/Activity9.java
@@ -2,23 +2,50 @@ package projectSelenium;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class Activity9 {
 	WebDriver driver;
 	
+	
 	  @Test
 	  public void f() {
 		  
 			
-			 driver.findElement(By.id("user_login")).sendKeys("root");
-			 driver.findElement(By.id("user_pass")).sendKeys("pa$$w0rd");
-			 driver.findElement(By.id("wp-submit")).click(); 
-			 
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+			driver.findElement(By.id("user_login")).sendKeys("root");
+			driver.findElement(By.id("user_pass")).sendKeys("pa$$w0rd");
+			driver.findElement(By.id("wp-submit")).click();
+			Actions act = new Actions(driver);
+			WebElement jobListing = driver.findElement(By.linkText("Job Listings"));
+			act.moveToElement(jobListing).build().perform();
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			WebElement addNew = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Add New")));
+			act.moveToElement(addNew).click().build().perform();
+			driver.findElement(By.id("post-title-0")).sendKeys("Test automation");
+			driver.findElement(By.id("_application")).clear();
+			driver.findElement(By.id("_application")).sendKeys("test@gmail.com");
+			driver.findElement(By.id("_company_website")).sendKeys("test.com");
+			driver.findElement(By.id("_company_twitter")).sendKeys("@test");
+			driver.findElement(By.id("_job_location")).sendKeys("India");
+			driver.findElement(By.id("_company_name")).sendKeys("Test Pvt Ltd");
+			driver.findElement(By.xpath(".//button[contains(text(), 'Publish…')]")).click();
+			driver.findElement(By.xpath(".//div[@class='editor-post-publish-panel__header-publish-button']/button")).click();
+			WebElement publishedText = driver.findElement(By.className("editor-post-publish-panel__header-published")); 
+			Assert.assertEquals(publishedText.getText(), "Published");
+			driver.findElement(By.xpath(".//div[@class='post-publish-panel__postpublish-buttons']/a")).click();
 			 
 			 	  
 	  }
